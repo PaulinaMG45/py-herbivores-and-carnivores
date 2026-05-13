ad0306daf2f9d7527aaae4723e2e43c03b58449d
Paulina Marin Gallego <paulinamarin3105@gmail.com>
2026-05-13 06:23:12 -0500

    Fix redundancy over the bite method

--- app/main.py
+++ app/main.py
@@ -39,7 +39,7 @@ class Herbivore(Animal):
 
 class Carnivore(Animal):
 
-    def bite(self, herbivore: Herbivore) -> None:
+    def bite(self, herbivore: Animal) -> None:
         if isinstance(herbivore, Herbivore) and herbivore.hidden is False:
             herbivore.health -= 50
             if herbivore.health <= 0:
